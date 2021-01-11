b736fa898dba04afd5e666667d16df8a51a78183
Dambaev Alexander <ice.redmine@gmail.com>
2021-01-11 15:18:55 +0800

    dont patch shebangs implicitely

--- derivation.nix
+++ derivation.nix
@@ -8,8 +8,13 @@ stdenv.mkDerivation {
       sha256 = "0mcyh5896a3frxqlkzqbdkskrpc5czgpkzcc12czmy9y7xvyy3ci";
     };
   buildInputs = with pkgs;
-  [ 
+  [ gnused
   ];
+  buildPhase = ''
+    sed -i 's/set -e/set -ex/' balenaos-in-container.sh
+    patchShebangs balenaos-in-containers.sh
+  '';
+  dontPatchShebangs = true;
   installPhase = ''
     mkdir -p $out/bin
     cp aufs2overlay.sh $out/bin
